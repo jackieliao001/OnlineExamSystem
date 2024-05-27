66d2e2be515767e9b7b32af82680ad6d423653cf
jackieliao001 <jackieliao001@163.com>
2024-05-27 14:08:48 +0800

    perf: 重构前端
    使用VUE3初始化前端工程

--- drama-web/src/components/icons/IconTooling.vue
+++ drama-web/src/components/icons/IconTooling.vue
@@ -2,6 +2,7 @@
 <template>
   <svg
       xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
       aria-hidden="true"
       role="img"
       class="iconify iconify--mdi"
--- drama-web/src/stores/counter.ts
+++ drama-web/src/stores/counter.ts
@@ -1,4 +1,4 @@
-import {computed, ref} from 'vue'
+import {ref, computed} from 'vue'
 import {defineStore} from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
